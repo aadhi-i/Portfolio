5811e3fc7d45479ea9c1a0443b43eacdc0ce0150
Adithya S Nair <adithyasnair25@gmail.com>
2024-09-19 10:55:58 +0530

    frontend completed

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    theme:{
+        gridTemplateColumns:{
+            'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+        },
+        fontFamily:{
+            Manrope:["Manrope", "sans-serif"]
+        }
+    }
+}
